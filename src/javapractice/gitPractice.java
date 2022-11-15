cc585b55697453360241752599aa2d4a561683c3
LemonGlacier <hijy02122@gmail.com>
2022-11-15 09:12:13 +0900

    branch1-실행1

--- src/javapractice/gitPractice.java
+++ src/javapractice/gitPractice.java
@@ -5,6 +5,7 @@ public class gitPractice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("project");
+		System.out.println("branch1-실행1");
 
 	}
 
